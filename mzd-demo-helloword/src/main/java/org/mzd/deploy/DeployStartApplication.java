f2de411e7754d53142e66a286b1164caab1df5df
牟振东 <mouzhendong.mzd@yungu-inc.org>
2022-05-17 17:28:52 +0800

    helloWord

---
+++ mzd-demo-helloword/src/main/java/org/mzd/deploy/DeployStartApplication.java
@@ -0,0 +1,55 @@
+package org.mzd.deploy;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalTime;
+
+/**
+ */
+@EnableAsync
+@ComponentScan(basePackages = {"org.mzd"})
+@RestController
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class DeployStartApplication {
+    private static final Logger logger = LoggerFactory.getLogger(DeployStartApplication.class);
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(DeployStartApplication.class, args);
+        } catch (Exception e) {
+            logger.error("DeployStartApplication startError:{}", e);
+        }
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner(ApplicationContext ctx) {
+        return args -> logger.info("=============="+ LocalTime.now()+"==============\n");
+    }
+    /**
+     * Hello，World
+     *
+     * @param who 参数，非必须
+     * @return Hello, ${who}
+     */
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(required = false, name = "who") String who) {
+        if (StringUtils.isBlank(who)) {
+            who = "World";
+        }
+        return String.format("Hello, {}!", who);
+    }
+
+}
